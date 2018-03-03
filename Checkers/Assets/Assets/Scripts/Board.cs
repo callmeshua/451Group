0bdbc5a81175867ddf96d74daff35a186d536ebb
callmeshua <callmeshua620@gmail.com>
2018-03-03 16:09:05 -0500

    prefabs and board starting script

--- Checkers/Assets/Assets/Scripts/Board.cs
+++ Checkers/Assets/Assets/Scripts/Board.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class Board : MonoBehaviour {
 
-    public Piece[][] board;
-    public Player[] Players;
+    public GameObject redPiece, whitePiece;
+    public GameObject[,] board = new GameObject[8, 8];
+    public Player[] players = new Player[2];
     int turn;
     int w, h;
 
 	// Use this for initialization
 	void Start () {
-		
+        setUpBoard();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void setUpBoard()
+    {
+        for(int i = 1; i <= board.GetLength(0); i++)
+        {
+            for(int j = 1; j <= board.GetLength(1); j += 2)
+            {
+                board[i-1, j-1] = (GameObject)Instantiate(redPiece, new Vector3(redPiece.transform.position.x + (j * 64), redPiece.transform.position.y + (i * 64),0), redPiece.transform.rotation);
+                Debug.Log(i + " " + j);
+            }
+        }
+    }
 
     public void replacePiece()
     {
