dec07aca713f792e3cd5bf193ca4b2fa9f36b28d
Jack Phoebus <jmp1996.jp@gmail.com>
2018-03-11 20:50:40 -0400

    clean

---
+++ Checkers/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer){
+			gameObject.AddComponent<LocalPlayer>();
+		}else{
+			gameObject.AddComponent<NetworkPlayer>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//send moves from local to server
+	[Command] public void CmdMakeMove(){
+	}
+
+	//recieve moves from server
+	[ClientRpc] void RpcMakeMove(){
+		//this will be called on ALL instances
+		//but the local instance is what sent the command in the first place
+		//we only want to recieve the command on a remote player
+		if(!isLocalPlayer){
+			
+		}
+	}
+}
